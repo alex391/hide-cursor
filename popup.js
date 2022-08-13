e34c1d1cdc92fe1116bf7d54c9e0a9e56f746343
Alex Leute <acleute@gmail.com>
2022-08-13 03:10:12 -0400

    minimum viable product!

---
+++ popup.js
@@ -0,0 +1,21 @@
+hide.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: hideCursor,
+    });
+});
+
+function hideCursor(){
+    console.log("hiding");
+    let elements = document.getElementsByTagName("*");
+    for(let i = 0; i < elements.length; i++){
+        if (elements[i].style.cursor == "none"){
+            elements[i].style.cursor = "auto";
+        }
+        else{
+            elements[i].style.cursor = "none";
+        }
+    }
+}
