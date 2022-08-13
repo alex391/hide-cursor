5dc399822978e9270303138596b6aa643bf57615
Alex Leute <acleute@gmail.com>
2022-08-13 16:53:14 -0400

    added !important to the css rules, and applied those rules to allFrames

--- popup.js
+++ popup.js
@@ -3,15 +3,15 @@ hide.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     let css = "";
     if (hidden) {
-        css = "* {cursor: auto}";
+        css = "* {cursor: auto !important}";
         hidden = false
     }
     else {
-        css = "* {cursor: none}"
+        css = "* {cursor: none !important}";
         hidden = true;
     }
     chrome.scripting.insertCSS({
-      target: { tabId: tab.id },
+      target: { tabId: tab.id, allFrames: true },
       css: css,
     });
 });
